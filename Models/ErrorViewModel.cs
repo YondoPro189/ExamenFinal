1bdbe5f400c95934a0c7047f7419f7cf5e53ecec
YondoPro189 <61927881+YondoPro189@users.noreply.github.com>
2025-12-01 19:56:07 -0600

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExamenFinal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamenFinal.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Career { get; set; }
+        public double Average { get; set; }
+    }
+}
+
